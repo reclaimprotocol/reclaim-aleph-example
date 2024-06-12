82bdf45adbf7b8b592c636211dd28d988bfae059
Hadi Saleh <hadi@creatoros.co>
2024-06-12 20:11:24 +0300

    add generate proof

--- src/components/GenerateProofPage/GenerateProofPage.tsx
+++ src/components/GenerateProofPage/GenerateProofPage.tsx
@@ -1,15 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Layout from 'components/Layout';
 import HeroHeading from 'components/HeroHeading';
 
 import { queries } from 'shared/layout';
 import { RootState } from 'redux/store';
-import { verifyProof } from 'utils/verifyProof';
 
-import { ApiPromiseType } from '../../App';
+
+import { Proof, Reclaim } from '@reclaimprotocol/js-sdk';
+import { setProof } from 'redux/slices/proofSlice';
+import QRCode from 'react-qr-code';
 
 const Wrapper = styled.div`
   color: ${({ theme }) => theme.colors.white};
@@ -94,25 +96,64 @@ const GenerateProofPage = (): JSX.Element => {
 
     const loggedAccount = useSelector((state: RootState) => state.walletAccounts.account);
 
-
+    const dispatch = useDispatch();
 
     const isButtonDisabled = (): boolean =>
         !(loggedAccount);
 
-    const handleVerifyProof = (): void => {
-        // if (!loggedAccount) return;
-        // if (!proof) return;
-        // if (api) verifyProof(proof, api, loggedAccount);
-    };
+
+    const [url, setUrl] = useState("");
+    const [ready, setReady] = useState(false);
+
+
+
+
+    async function generateVerificationRequest() {
+        const reclaimClient = new Reclaim.ProofRequest(""); // TODO: replace with your applicationId
+        const providerId = ""; // TODO: replace with your provider ids you had selected while creating the application
+
+        reclaimClient.addContext(
+            `user's address`,
+            "for acmecorp.com on 1st january"
+        );
+
+        await reclaimClient.buildProofRequest(providerId);
+
+        reclaimClient.setSignature(
+            await reclaimClient.generateSignature(
+                "" // TODO : replace with your APP_SECRET
+            )
+        );
+
+        const { requestUrl, statusUrl } =
+            await reclaimClient.createVerificationRequest();
+
+        setUrl(requestUrl);
+
+        await reclaimClient.startSession({
+            onSuccessCallback: (proofs: Proof[]) => {
+                console.log("Verification success", proofs);
+                setReady(true);
+                dispatch(setProof(proofs[0]));
+                // Your business logic here
+            },
+            onFailureCallback: (error: Error) => {
+                console.error("Verification failed", error);
+                // Your business logic here to handle the error
+            },
+        });
+    }
 
     return (
         <Layout>
             <Wrapper className="wrapper">
                 <HeroHeading variant="generate" />
                 <div className="post-form">
-                    <button type="button" disabled={isButtonDisabled()} onClick={handleVerifyProof}>
-                        Verify Proof
-                    </button>
+                    {!url && <button type="button" disabled={isButtonDisabled()} onClick={generateVerificationRequest}>
+                        Generate Proof
+                    </button>}
+                    {url && <QRCode value={url} />}
+                    {ready && <span> Proof is ready! Go to Verify Proof Page to verify it..</span>}
                 </div>
             </Wrapper>
         </Layout>
